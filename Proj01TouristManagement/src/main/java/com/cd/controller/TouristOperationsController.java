7f1528722fb9b7381e239af357472a0ee80010b2
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-27 23:37:23 +0530

    method path added

--- Proj01TouristManagement/src/main/java/com/cd/controller/TouristOperationsController.java
+++ Proj01TouristManagement/src/main/java/com/cd/controller/TouristOperationsController.java
@@ -2,6 +2,7 @@ package com.cd.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class TouristOperationsController {
 		this.service = service;
 	}
 	
+	@PostMapping("/register")
 	public ResponseEntity<String> enrollTourist(@RequestBody Tourist tourist){
 		try {
 			String resultMsg=service.registerTourost(tourist);
