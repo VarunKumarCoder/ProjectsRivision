1edb7f05b54b70378b78dfe075a9c2ed2a232e01
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-27 23:36:45 +0530

    api end point created

---
+++ Proj01TouristManagement/src/main/java/com/cd/controller/TouristOperationsController.java
@@ -0,0 +1,30 @@
+package com.cd.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cd.entity.Tourist;
+import com.cd.service.ITouristMgmtService;
+
+@RestController
+public class TouristOperationsController {
+
+	private ITouristMgmtService service;
+
+	public TouristOperationsController(ITouristMgmtService service) {
+		super();
+		this.service = service;
+	}
+	
+	public ResponseEntity<String> enrollTourist(@RequestBody Tourist tourist){
+		try {
+			String resultMsg=service.registerTourost(tourist);
+			return new ResponseEntity<String>(resultMsg,HttpStatus.CREATED);
+		} catch (Exception e) {
+			return new ResponseEntity<String>("Tourist is Not registed due to Technical Issues",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+				
+	}
+}
