ca1a9a260795484958ed97d53c2965991175dceb
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-28 17:49:06 +0530

    actor entity class is created

---
+++ Proj02ActorManagemet/src/main/java/com/cd/Proj02ActorManagemetApplication.java
@@ -0,0 +1,13 @@
+package com.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj02ActorManagemetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj02ActorManagemetApplication.class, args);
+	}
+
+}
---
+++ Proj02ActorManagemet/src/main/java/com/cd/entity/ActorInfo.java
@@ -0,0 +1,54 @@
+package com.cd.entity;
+
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.lang.NonNull;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+@Entity
+@Table(name="ACTOR_INFO")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Builder
+public class ActorInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="ID", updatable = false, nullable = false)
+	private Integer id;
+	@NonNull
+	@Column(name="ACTOR_NAME",length = 60)
+	private String aname;
+	@NonNull
+	@Column(name="ACTOR_ADDRESS",length=150)
+	private String addrs;
+	@NonNull()
+	@Column(name="ACTIVE_SWITCH",length=10)
+	private String active_sw;
+	//META DATA
+	@Column(length=60)
+	private String createdBy;
+	@Column(length=60)
+	private String updatedBy;
+	@Column(updatable=false,insertable=true)
+	@CreationTimestamp
+	private LocalDate creationDate;
+	@UpdateTimestamp
+	@Column(insertable=false,updatable=true)
+	private LocalDate updationDate;
+}
