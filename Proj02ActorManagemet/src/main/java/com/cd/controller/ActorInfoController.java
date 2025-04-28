f047d2af46778d5c363d99428ef82aa5ebf27946
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-28 18:35:34 +0530

    postmapping for save

--- Proj02ActorManagemet/src/main/java/com/cd/controller/ActorInfoController.java
+++ Proj02ActorManagemet/src/main/java/com/cd/controller/ActorInfoController.java
@@ -1,5 +1,29 @@
 package com.cd.controller;
 
-public class ActorInfoController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.cd.model.ActorData;
+import com.cd.service.IActorInfoMgmt;
+
+@RestController
+@RequestMapping("/actorinfo-api")
+public class ActorInfoController {
+	
+	private IActorInfoMgmt service;
+
+	public ActorInfoController(IActorInfoMgmt service) {
+		super();
+		this.service = service;
+	}
+
+	@PostMapping("/registeractor")
+	public ResponseEntity<String> saveActor(@RequestBody ActorData data){
+		String msg=service.registerActorinfo(data);
+		return new ResponseEntity<String>(msg,HttpStatus.OK);
+	}
 }
