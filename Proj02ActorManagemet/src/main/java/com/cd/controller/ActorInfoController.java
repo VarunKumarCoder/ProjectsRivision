da80c3122addb0b323b04870248b0b1d2e0674a4
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-28 23:42:33 +0530

    miniproj one

--- Proj02ActorManagemet/src/main/java/com/cd/controller/ActorInfoController.java
+++ Proj02ActorManagemet/src/main/java/com/cd/controller/ActorInfoController.java
@@ -1,7 +1,12 @@
 package com.cd.controller;
 
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +31,32 @@ public class ActorInfoController {
 		String msg=service.registerActorinfo(data);
 		return new ResponseEntity<String>(msg,HttpStatus.OK);
 	}
+	
+	
+	@GetMapping("/findallactors")
+	public ResponseEntity<List<ActorData>> findAllActors(){
+		List<ActorData> data=service.showAllActors();
+		return new ResponseEntity<List<ActorData>>(data,HttpStatus.OK);
+	}
+	
+	
+	@GetMapping("/find/{id}")
+	public ResponseEntity<ActorData> findById(@PathVariable Integer id){
+		ActorData data=service.showActorById(id);
+		return new ResponseEntity<ActorData>(data,HttpStatus.OK);
+	}
+	
+	@PostMapping("/update")
+	public ResponseEntity<String> updateInfo(@RequestBody ActorData data){
+		String msg=service.updateActorData(data);
+		return new ResponseEntity<String>(msg,HttpStatus.OK);
+	}
+	
+	@PatchMapping("/rupdate/{id}/{amount}")
+	public ResponseEntity<String> updateActorRemuneration(@PathVariable("id") Integer id,@PathVariable("amount") double amount) {
+		String msg=service.updateActorRemuneration(id, amount);
+		return new ResponseEntity<String>(msg,HttpStatus.OK);
+	}
+	
+	
 }
--- Proj02ActorManagemet/src/main/java/com/cd/service/ActorInfoMgmtImpl.java
+++ Proj02ActorManagemet/src/main/java/com/cd/service/ActorInfoMgmtImpl.java
@@ -1,6 +1,8 @@
 package com.cd.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Value;
@@ -19,7 +21,7 @@ public class ActorInfoMgmtImpl implements IActorInfoMgmt {
 	private IActorRepo repo;
 	
 	public ActorInfoMgmtImpl(IActorRepo repo) {
-		super();
+	
 		this.repo = repo;
 	}
 
@@ -39,26 +41,45 @@ public class ActorInfoMgmtImpl implements IActorInfoMgmt {
 
 	@Override
 	public List<ActorData> showAllActors() {
-		// TODO Auto-generated method stub
-		return null;
+		//call original method using entity class
+		List<ActorInfo> list=repo.findAll();
+		//convert entity into model class
+		List<ActorData> modeldata=new ArrayList();
+		//print using enhanced for loop
+		for(ActorInfo entity:list) {
+			//create object to store the list of model data
+			ActorData adata=new ActorData();
+			//finded entity data using predefined class now copy entity into model 
+			BeanUtils.copyProperties(entity, adata);
+			//after copying add data to object
+			modeldata.add(adata);
+		}
+		return modeldata;
 	}
 
 	@Override
 	public ActorData showActorById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		ActorInfo info=repo.findById(id).orElseThrow(()->new IllegalArgumentException("invalid ID"));
+		ActorData data=new ActorData();
+		BeanUtils.copyProperties(info, data);
+		return data;
 	}
 
 	@Override
 	public String updateActorData(ActorData data) {
-		// TODO Auto-generated method stub
-		return null;
+		ActorInfo info=repo.findById(data.getId()).orElseThrow(()->new IllegalArgumentException("invalid ID"));
+		BeanUtils.copyProperties(data, info);
+		info.setUpdatedBy(osuser);
+		int idVal=repo.save(info).getId();
+		return idVal+" ActorInfo is updated";
 	}
 
 	@Override
 	public String updateActorRemuneration(int id, double amount) {
-		// TODO Auto-generated method stub
-		return null;
+		ActorInfo info=repo.findById(id).orElseThrow(()->new IllegalArgumentException("invalid ID"));
+		info.setRemuneration(amount);
+		int idVal=repo.save(info).getId();
+		return idVal+" is updated";
 	}
 
 	@Override
