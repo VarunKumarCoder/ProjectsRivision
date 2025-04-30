e2a62c2a530e3f12054b8a36e4c692e71c7ecaf4
VarunKumarCoder <medavarunkumar@gmail.com>
2025-04-30 18:08:20 +0530

    global exception class developed

--- Proj05KafkaMiniProjni/src/main/java/com/cd/advice/KafkaMessageOperationsControllerAdvice.java
+++ Proj05KafkaMiniProjni/src/main/java/com/cd/advice/KafkaMessageOperationsControllerAdvice.java
@@ -1,5 +1,15 @@
 package com.cd.advice;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class KafkaMessageOperationsControllerAdvice {
 
+	@ExceptionHandler(exception = Exception.class)
+	public ResponseEntity<String> handleAllExceptions(Exception e){
+		return new ResponseEntity<String>(e.getMessage(),HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
