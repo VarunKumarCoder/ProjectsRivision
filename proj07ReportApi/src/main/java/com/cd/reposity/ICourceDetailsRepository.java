30abedf4a36a4d4b2c5eec86000c7165ac601222
VarunKumarCoder <medavarunkumar@gmail.com>
2025-05-14 22:06:56 +0530

    repository created

---
+++ proj07ReportApi/src/main/java/com/cd/reposity/ICourceDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.cd.reposity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cd.entity.CourceDetails;
+
+public interface ICourceDetailsRepository extends JpaRepository<CourceDetails, Integer> {
+
+}
