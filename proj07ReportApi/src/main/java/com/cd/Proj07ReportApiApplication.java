eccae0c9101ae7f5412cc67cdb8d09b3db384682
VarunKumarCoder <medavarunkumar@gmail.com>
2025-05-14 21:48:18 +0530

    Initial commit

---
+++ proj07ReportApi/src/main/java/com/cd/Proj07ReportApiApplication.java
@@ -0,0 +1,13 @@
+package com.cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proj07ReportApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proj07ReportApiApplication.class, args);
+	}
+
+}
