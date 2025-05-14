64756cded36e6e1342915724329b52c4f3040799
VarunKumarCoder <medavarunkumar@gmail.com>
2025-05-14 22:30:57 +0530

    couce inputs binding class done

---
+++ proj07ReportApi/src/main/java/com/cd/model/SearchInputs.java
@@ -0,0 +1,17 @@
+package com.cd.model;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class SearchInputs {
+	private String   courseCategory;
+	private  String   trainingMode;
+	private  String   facultyName;
+	private  LocalDateTime  startsOn;
+}
